146f6e8a36f5ca27ef9c5d78098608783af96149
HYIIM <haya.m.alsa@gmail.com>
2024-10-20 22:41:57 +0300

    Initial Commit

---
+++ Hydrate app/Hydrate_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hydrate_appApp.swift
+//  Hydrate app
+//
+//  Created by HY on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Hydrate_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hydrate appUITests/Hydrate_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hydrate_appUITestsLaunchTests.swift
+//  Hydrate appUITests
+//
+//  Created by HY on 17/04/1446 AH.
+//
+
+import XCTest
+
+final class Hydrate_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
