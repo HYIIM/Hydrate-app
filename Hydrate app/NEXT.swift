ddc784efd90110df71a11471dbfae68aa0658ddb
HYIIM <haya.m.alsa@gmail.com>
2024-10-22 11:03:32 +0300

    changes

---
+++ Hydrate app/NEXT.swift
@@ -0,0 +1,18 @@
+//
+//  NEXT.swift
+//  Hydrate app
+//
+//  Created by HY on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+struct NEXT: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NEXT()
+}
